147f7c2eaf68b043eb264b3941fc7dbd6fb5a543
Adrian Cardona Candil <adriancardona@MacBook-Air-de-Adrian.local>
2024-11-04 19:37:55 +0000

    column on additionalcontainer for content page done

--- app/src/main/java/com/example/anitrack/ui/content/AdditionalInfoContainer.kt
+++ app/src/main/java/com/example/anitrack/ui/content/AdditionalInfoContainer.kt
@@ -1,27 +1,16 @@
 package com.example.anitrack.ui.content
 
-import androidx.compose.foundation.border
-import androidx.compose.foundation.horizontalScroll
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ExperimentalLayoutApi
-import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -35,8 +24,7 @@ import androidx.compose.ui.unit.dp
 * averageDuration: Int,
 * rating: String,
 * season: String,
-* year: Int,
-* studios: List<String>
+* year: Int
 * */
 
 @Composable
@@ -51,104 +39,60 @@ fun AdditionalInfoContainer(
     averageDuration: Int = 0,
     rating: String = "defaultRating",
     season: String = "defaultSeason",
-    year: Int = 0,
-    studios: List<String> = listOf("studio1", "studio2")
+    year: Int = 0
 ){
     Column(modifier = modifier) {
-        AdditionalInfoLabel(
-            label = "Title",
-            content = originalTitle
+        val infoItems = listOf(
+            "Title" to originalTitle,
+            "Romaji" to romajiTitle,
+            "English" to englishTitle,
+            "Source" to source,
+            "Aired From" to airedFrom,
+            "Aired To" to airedTo,
+            "Duration per episode" to "$averageDuration m",
+            "Rating" to rating,
+            "Season" to season,
+            "Year" to year
         )
-        AdditionalInfoLabel(
-            label = "Romaji",
-            content = romajiTitle
-        )
-        AdditionalInfoLabel(
-            label = "English",
-            content = englishTitle
-        )
-        AdditionalInfoLabel(
-            label = "Source",
-            content = source
-        )
-        AdditionalInfoLabel(
-            label = "Aired From",
-            content = airedFrom
-        )
-        AdditionalInfoLabel(
-            label = "Aired To",
-            content = airedTo
-        )
-        AdditionalInfoLabel(
-            label = "Duration per episode",
-            content = "${averageDuration}m"
-        )
-        AdditionalInfoLabel(
-            label = "Rating",
-            content = rating
-        )
-        AdditionalInfoLabel(
-            label = "Season",
-            content = season
-        )
-        AdditionalInfoLabel(
-            label = "Year",
-            content = year.toString()
-        )
-        StudiosList(
-            label = "Studios",
-            studios = studios,
-            modifier = Modifier.padding(vertical = 8.dp).horizontalScroll(rememberScrollState())
-        )
-    }
-}
-
-@OptIn(ExperimentalLayoutApi::class)
-@Composable
-fun StudiosList(
-    label: String,
-    studios: List<String>,
-    modifier: Modifier = Modifier,
-){
-    FlowRow (
-        modifier = modifier,
-        maxLines = 1
-    ) {
-        Text(
-            text = "$label:   ",
-            style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
-            color = MaterialTheme.colorScheme.secondary
-        )
-        studios.forEach { item ->
-            Text(
-                text = item,
-                style = MaterialTheme.typography.bodyLarge,
-                color = MaterialTheme.colorScheme.secondary,
-                modifier = Modifier.padding(end = 5.dp)
+        infoItems.forEach { (label, value) ->
+            AdditionalInfoLabel(
+                label = label,
+                value = value.toString(),
+                modifier = Modifier.padding(vertical = 10.dp)
             )
         }
     }
 }
 
+
 @Composable
 fun AdditionalInfoLabel(
     label: String,
-    content: String,
+    value: String,
     modifier: Modifier = Modifier
 ){
-    Text(
-        text = buildAnnotatedString {
-            withStyle(style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold).toSpanStyle()) {
-                append("$label:   ")
-            }
-            withStyle(style = MaterialTheme.typography.bodyLarge.toSpanStyle()) {
-                append(content)
-            }
-        },
-        maxLines = 2,
-        color = MaterialTheme.colorScheme.secondary,
-        overflow = TextOverflow.Ellipsis,
-        modifier = modifier.padding(vertical = 8.dp).fillMaxWidth()
+    Row(modifier = modifier) {
+        Text(
+            text = "$label:",
+            modifier = Modifier
+                .weight(1f),
+            color = MaterialTheme.colorScheme.secondary,
+            style = MaterialTheme.typography.bodyLarge,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = value,
+            modifier = Modifier
+                .weight(1f),
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis,
+            color = MaterialTheme.colorScheme.secondary,
+            style = MaterialTheme.typography.bodyLarge
+        )
+    }
+    HorizontalDivider(
+        thickness = 0.5.dp,
+        color = MaterialTheme.colorScheme.tertiary
     )
 }
 
