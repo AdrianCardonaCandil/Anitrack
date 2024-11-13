cea0119e6ca95cfeb31dc4a751713d0c150dcf61
ElenaMoralesGil <elena.morales104@alu.ulpgc.es>
2024-11-13 21:26:44 +0000

    merge with adrian changes

--- app/src/main/java/com/example/anitrack/ui/AnitrackApp.kt
+++ app/src/main/java/com/example/anitrack/ui/AnitrackApp.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
