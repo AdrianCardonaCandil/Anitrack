996757520301df6b14a198d60d0519892d9f8964
elenamoralesgil <elena.morales104@alu.ulpgc.es>
2024-11-25 20:58:59 +0000

    added gradle to ComposeQuadrantApp

--- ComposeQuadrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
+++ ComposeQuadrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -25,6 +25,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.composeQuadrant.R
 import com.example.composequadrant.ui.theme.ComposeQuadrantTheme
 
 class MainActivity : ComponentActivity() {
